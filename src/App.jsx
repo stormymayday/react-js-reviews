aac18a92c5e3f37cb1c97faa2da67c95ad482ad6
stormymayday <stormymayday7@gmail.com>
2023-12-23 20:51:45 -0800

    importing data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import data from "./data";
+
 const App = () => {
+    const [reviews, setReviews] = useState(data);
+
+    console.log(reviews);
+
     return <h2>Reviews Starter</h2>;
 };
 export default App;
