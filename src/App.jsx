7198e095ed1ade7e87c2acdbeddc76c2869610f4
stormymayday <stormymayday7@gmail.com>
2023-12-27 20:03:56 -0800

    Rendering the first 'person'

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
 import people from "./data";
+import {
+    FaChevronCircleLeft,
+    FaChevronCircleRight,
+    FaQuoteRight,
+} from "react-icons/fa";
 
 const App = () => {
     const [index, setIndex] = useState(0);
 
     const { name, job, image, text } = people[index];
 
-    console.log(name);
-
-    return <h2>Reviews Starter</h2>;
+    return (
+        <main>
+            <article className="review">
+                <div className="img-container">
+                    <img className="person-img" src={image} alt={name} />
+                    <span className="quote-icon">
+                        <FaQuoteRight />
+                    </span>
+                </div>
+                <h4 className="author">{name}</h4>
+                <p className="job">{job}</p>
+                <p className="info">{text}</p>
+            </article>
+        </main>
+    );
 };
 export default App;
