b7ba5f9dd75f26f321c6cd10594f62bd06863ad2
stormymayday <stormymayday7@gmail.com>
2023-12-27 22:43:31 -0800

    Implemented 'prev' and 'next' button functionality using simple if statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from "react";
 import people from "./data";
-import {
-    FaChevronCircleLeft,
-    FaChevronCircleRight,
-    FaQuoteRight,
-} from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const App = () => {
     const [index, setIndex] = useState(0);
 
     const { name, job, image, text } = people[index];
+
+    const nextPerson = () => {
+        if (index !== people.length - 1) {
+            // setIndex(index + 1);
+
+            // Functional approach:
+            setIndex((currentIndex) => {
+                const newIndex = currentIndex + 1;
+                return newIndex;
+            });
+        } else {
+            setIndex(0);
+        }
+    };
+    const prevPerson = () => {
+        if (index > 0) {
+            // setIndex(index - 1);
+
+            // Functional approach:
+            setIndex((currentIndex) => currentIndex - 1);
+        } else {
+            setIndex(people.length - 1);
+        }
+    };
 
     return (
         <main>
@@ -23,6 +43,22 @@ const App = () => {
                 <h4 className="author">{name}</h4>
                 <p className="job">{job}</p>
                 <p className="info">{text}</p>
+                <div className="btn-container">
+                    <button
+                        className="prev-btn"
+                        type="button"
+                        onClick={prevPerson}
+                    >
+                        <FaChevronLeft />
+                    </button>
+                    <button
+                        className="next-btn"
+                        type="button"
+                        onClick={nextPerson}
+                    >
+                        <FaChevronRight />
+                    </button>
+                </div>
             </article>
         </main>
     );
