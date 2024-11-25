bfc2bf86b0b2367e30d8eb0e82ff2a276cfc91a4
stormymayday <stormymayday7@gmail.com>
2024-11-24 16:47:26 -0800

    Modulus Operator

--- utils/index.ts
+++ utils/index.ts
@@ -2,22 +2,24 @@ export const getNextCyclicIndex = (
     currentIndex: number,
     arrayLength: number
 ): number => {
-    if (currentIndex < arrayLength - 1) {
-        return currentIndex + 1;
-    } else {
-        return 0;
-    }
+    // if (currentIndex < arrayLength - 1) {
+    //     return currentIndex + 1;
+    // } else {
+    //     return 0;
+    // }
+    return (currentIndex + 1) % arrayLength;
 };
 
 export const getPreviousCyclicIndex = (
     currentIndex: number,
     arrayLength: number
 ): number => {
-    if (currentIndex > 0) {
-        return currentIndex - 1;
-    } else {
-        return arrayLength - 1;
-    }
+    // if (currentIndex > 0) {
+    //     return currentIndex - 1;
+    // } else {
+    //     return arrayLength - 1;
+    // }
+    return (currentIndex - 1 + arrayLength) % arrayLength;
 };
 
 export const getRandomIndex = (
